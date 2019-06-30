f4e0df0eba775278535a85e143319fab39b20a09
Yanis <yanisakli@yahoo.fr>
2019-06-30 22:42:29 +0200

    :tada:  Project added successfully :tada:

--- MarocIos/AddProjectController.swift
+++ MarocIos/AddProjectController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class AddProjectController: UIViewController {
-
+    
+    var refProjects:DatabaseReference!
+    var isPrivate = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewComponents()
+        refProjects = Database.database().reference().child("projects")
     }
-    
+    @objc func addProject(){
+        let key = refProjects.childByAutoId().key
+        let project = [ "id" : key,
+                       "information" : informationTextField.text! as String,
+                       "date" : dateTextField.text! as String,
+                       "teamName" : teamNameTextField.text! as String,
+                       "subject" : subjectTextField.text! as String,
+                       "isPrivate" : isPrivate ? "true" : "false"
+                        ]
+        
+        refProjects.child(key!).setValue(project)
+        navigationController?.popViewController(animated: true)
+    }
+
     lazy var informationContainerView: UIView = {
         let view = UIView()
         return view.textContainerView(view: view, #imageLiteral(resourceName: "icons8-information-50"), informationTextField)
@@ -62,13 +82,23 @@ class AddProjectController: UIViewController {
         return tf
     }()
     
+    let switchButton: UISwitch = {
+        let myswitch = UISwitch()
+        myswitch.isOn = false
+        myswitch.thumbTintColor = UIColor.black
+        myswitch.tintColor = UIColor.green
+        myswitch.onTintColor = UIColor.cyan
+        myswitch.addTarget(self, action: #selector(switchInAction(sender:)), for: UIControl.Event.valueChanged)
+        return myswitch
+    }()
+    
     let loginButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Add", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
         button.setTitleColor(UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0), for: .normal)
         button.backgroundColor = UIColor(red:0.40, green:0.37, blue:1.00, alpha:1.0)
-        button.addTarget(self, action: #selector(handleCreateProject), for: .touchUpInside)
+        button.addTarget(self, action: #selector(addProject), for: .touchUpInside)
         button.layer.cornerRadius = 10
         return button
     }()
@@ -84,25 +114,18 @@ class AddProjectController: UIViewController {
         return buttonOne
     }()
     
-    @objc func handleCreateProject() {
-//        guard let information = informationTextField.text else { return }
-//        guard let teamName = teamNameTextField.text else { return }
-//        guard let date = dateTextField.text else { return }
-//        guard let subject = subjectTextField.text else { return }
-        
-        createProject()
+    @objc func switchInAction(sender : UISwitch) {
+        if(sender.isOn){
+            isPrivate = true
+        }
+        else{
+            isPrivate = false
+        }
     }
     
     @objc func handleShowProjects() {
         navigationController?.popViewController(animated: true)
     }
-    
-    // MARK: - API
-    
-    func createProject() {
-    }
-    
-    // MARK: - Helper Functions
     
     func configureViewComponents() {
         view.backgroundColor = UIColor(red:0.16, green:0.18, blue:0.26, alpha:1.0)
@@ -123,8 +146,11 @@ class AddProjectController: UIViewController {
         view.addSubview(privateProjectLabel)
         privateProjectLabel.anchor(top: subjectContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 30, paddingLeft: 40, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
         
+        view.addSubview(switchButton)
+        switchButton.anchor(top: subjectContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 330, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        
         view.addSubview(loginButton)
-        loginButton.anchor(top: privateProjectLabel.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 100, paddingLeft: 32, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
+        loginButton.anchor(top: switchButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 100, paddingLeft: 32, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
     
         view.addSubview(AbortButton)
         AbortButton.anchor(top: loginButton.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 30, paddingLeft: 32, paddingBottom: 0, paddingRight: 32, width: 0, height: 50)
