95cc8c3d02b4f4aab2d38059479d017dd7a7cf92
Yanis <yanisakli@yahoo.fr>
2019-07-24 17:20:30 +0200

    :construction: list of friends

--- MarocIos/FriendsController.swift
+++ MarocIos/FriendsController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import Firebase
+
+class FriendModal {
+    var name: String?
+    var email: String?
+    var id: String?
+    
+    init( name: String, email: String, id: String) {
+        self.name = name
+        self.email = email
+        self.id = id
+    }
+}
 
 class FriendsController: UIViewController {
 
+    var tableView = UITableView()
+    var userArr = [FriendModal]()
+    var idProject : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewComponents()
+        setTableView()
+        
+        let refTasks = Database.database().reference().child("users")
+        refTasks.observe(DataEventType.value, with: {(snapshot) in
+            if snapshot.childrenCount > 0 {
+                self.userArr.removeAll()
+                
+                for friends in snapshot.children.allObjects as! [DataSnapshot]{
+                    
+                    let friendObject = friends.value as? [String: String]
+                    let friendName = (friendObject?["name"])! + " " + (friendObject?["familyName"])!
+                    let friendEmail = friendObject?["email"]
+                    let friendId = friends.key 
+                    
+                    let friend = FriendModal(name: (friendName ), email: (friendEmail )!, id: (friendId ));
+                    self.userArr.append(friend)
+                }
+                self.tableView.reloadData()
+            }
+            
+        })
     }
 
+    
+    
+    
+    
+    
     let AbortButton: UIButton = {
         let buttonOne = UIButton(type: .system)
         buttonOne.setTitle("Back", for: .normal)
@@ -26,6 +69,21 @@ class FriendsController: UIViewController {
         return buttonOne
     }()
     
+    func setTableView(){
+        tableView.frame = self.view.frame
+        tableView.backgroundColor = UIColor.clear
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorColor = UIColor.clear
+        tableView.backgroundColor = UIColor(red:0.16, green:0.18, blue:0.26, alpha:1.0)
+        self.view.addSubview(tableView)
+        
+        
+        self.view.addSubview(AbortButton)
+        AbortButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 32, paddingBottom: 100, paddingRight: 32, width: 0, height: 50)
+        
+        tableView.register(FriendsTableViewCell.self, forCellReuseIdentifier: "CellFriends")
+    }
     
     @objc func handleShowSettings() {
         navigationController?.popViewController(animated: true)
@@ -34,11 +92,37 @@ class FriendsController: UIViewController {
     func configureViewComponents() {
         view.backgroundColor = UIColor(red:0.16, green:0.18, blue:0.26, alpha:1.0)
         navigationController?.navigationBar.isHidden = true
-        
-        view.addSubview(AbortButton)
-        AbortButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 32, paddingBottom: 100, paddingRight: 32, width: 0, height: 50)
-        
     }
-
     
 }
+
+extension FriendsController : UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CellFriends", for: indexPath) as? FriendsTableViewCell else {fatalError("Unabel to create cell")}
+        cell.namelbl.text = userArr[indexPath.row].name
+        cell.emaillbl.text = userArr[indexPath.row].email
+        cell.idFriendlbl.text = userArr[indexPath.row].id
+        
+        cell.cellDelegate = self
+        cell.index = indexPath
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+
+extension FriendsController : FriendsTableViewNew {
+    func onClickCell(index : Int, idFriend : String){
+        Database.database().reference().child("users").child("\(idFriend)").removeValue()
+    }
+    
+}
+
--- MarocIos/FriendsTableViewCell.swift
+++ MarocIos/FriendsTableViewCell.swift
@@ -8,17 +8,80 @@
 
 import UIKit
 
-class FriendsTableViewCell: UITableViewCell {
+protocol FriendsTableViewNew {
+    func onClickCell(index : Int, idFriend: String)
+}
 
+class FriendsTableViewCell: UITableViewCell {
+    
+    var cellDelegate : FriendsTableViewNew?
+    var index : IndexPath?
+    var idFriend : String?
+
+    lazy var backView: UIView = {
+        let view = UIView(frame: CGRect(x: 10, y: 6, width: self.frame.width - 20, height: 110))
+        view.backgroundColor = UIColor(red:0.40, green:0.37, blue:1.00, alpha:1.0)
+        
+        return view
+    }()
+    
+    lazy var namelbl: UILabel = {
+        let lbl = UILabel(frame: CGRect(x: 50, y: 10, width: backView.frame.width - 116, height: 30))
+        lbl.textAlignment = .left
+        lbl.font = UIFont.boldSystemFont(ofSize: 18)
+        return lbl
+    }()
+    
+    lazy var emaillbl: UILabel = {
+        let lbl = UILabel(frame: CGRect(x: 50, y: 50, width: backView.frame.width - 116, height: 30))
+        lbl.textAlignment = .left
+        return lbl
+    }()
+    
+    lazy var idFriendlbl: UILabel = {
+        let lbl = UILabel(frame: CGRect(x: 50, y: 50, width: backView.frame.width - 116, height: 30))
+        lbl.textAlignment = .left
+        return lbl
+    }()
+    
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Add", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = UIColor.cyan
+        button.layer.cornerRadius = 10
+        
+        return button
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    override func layoutSubviews() {
+        contentView.backgroundColor = UIColor.clear
+        backgroundColor = UIColor.clear
+        backView.layer.cornerRadius = 5
+        backView.clipsToBounds = true
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        addSubview(backView)
+        backView.addSubview(namelbl)
+        backView.addSubview(emaillbl)
+        backView.addSubview(actionButton)
+        
+        actionButton.anchor(top: backView.topAnchor, left: backView.leftAnchor, bottom: nil, right: backView.rightAnchor, paddingTop: 75, paddingLeft: 320, paddingBottom: 0, paddingRight: 10, width: 0, height: 0)
+        actionButton.addTarget(self, action: #selector(handleDetailProjects), for: .touchUpInside)
+        
+    }
+    
+    @objc func handleDetailProjects() {
+        cellDelegate?.onClickCell(index: (index?.row)!,  idFriend: idFriendlbl.text!)
     }
 
 }
